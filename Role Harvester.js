59164a5eeb24294fb15c7ad9f6eb236e1122a3a5
Yuratul <Yostroukhov@gmail.com>
2022-10-11 18:42:45 -0700

    Update Role Harvester.js

--- Role Harvester.js
+++ Role Harvester.js
@@ -8,8 +8,21 @@ module.exports = {
         let container = creep.pos.findClosestByPath(FIND_STRUCTURES, {
         	   filter: (c) => c.structureType == STRUCTURE_CONTAINER && c.store[RESOURCE_ENERGY] <= c.storeCapacity
     	});
+    	let source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE)
+    	let helper = creep.pos.findClosestByPath(FIND_MY_CREEPS, {
+        	   filter: (c) => c.memory.role == "Courier"});
     	if(creep.memory.working == false) {
-    	    if(creep.memory.source)
+    	    if(creep.harvest(source) == ERR_NOT_IN_RANGE){
+    	        creep.memory.help = true;
+    	        creep.move(helper)
+    	        creep.say(creep.move(helper))
+    	    } else {
+    	        creep.harvest(source)
+    	        creep.memory.help = false;
+    	    }
+    	}
+    	if(helper.memory.working == false && creep.transferEnergy){
+    	    return
     	}
 	}
 };
