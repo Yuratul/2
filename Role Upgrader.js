14492badefdfad2f8906f987bb0e9d6b5d07f472
Yuratul <Yostroukhov@gmail.com>
2022-10-12 08:11:27 -0700

    Create Role Upgrader.js

---
+++ Role Upgrader.js
@@ -0,0 +1,21 @@
+module.exports = {
+	run: function(creep) {
+        let container = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+        	   filter: (c) => c.structureType == STRUCTURE_CONTAINER && c.store[RESOURCE_ENERGY] <= c.storeCapacity
+    	});
+    	let source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE)
+    	let helper = creep.pos.findClosestByPath(FIND_MY_CREEPS, {
+        	   filter: (c) => c.memory.role == "Courier"});
+        let controller = creep.room.controller
+    	if(creep.memory.working == false) {
+    	    if(creep.upgrade(controller)== ERR_NOT_IN_RANGE) {
+    	        creep.memory.help = true;
+    	        creep.move(helper)
+    	        creep.say(creep.move(helper))
+    	    } else {
+    	        creep.upgrade(controller)
+    	        creep.memory.help = false;
+    	    }
+    	} 
+	}
+};
