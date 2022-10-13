44e9cee207d6973b34fe0ebbe95b722f1c2b7194
Yuratul <Yostroukhov@gmail.com>
2022-10-13 08:09:52 -0700

    Update main.js

--- main.js
+++ main.js
@@ -3,12 +3,9 @@ module.exports.loop = function () {
     let Harvester = require("Role Harvester")
     let Courier = require("Role Courier")
     let Upgrader = require("Role Upgrader")
-    for(let i in Memory.creeps) {
-        if(!Game.creeps[i]) {
-            delete Memory.creeps[i];
-        }
-    }
-    for(let spawn in Game.spawns){
+    for(let spawnName in Game.spawns){
+        
+        
         for(let creep in Game.creeps){
             creep = Game.creeps[creep]
             
@@ -20,13 +17,14 @@ module.exports.loop = function () {
                 Upgrader.run(creep)
             }
         }
-        spawn = Game.spawns[spawn]
+        
+        spawn = Game.spawns[spawnName]
+        room = spawn.room
         let creepsInRoom = spawn.room.find([FIND_MY_CREEPS])
         let numberOfHarvesters = _.sum(creepsInRoom, (c) => c.memory.role == 'Harvester');
         let numberOfUpgraders = _.sum(creepsInRoom, (c) => c.memory.role == 'Upgrader');
         let numberOfCouriers = _.sum(creepsInRoom, (c) => c.memory.role == 'Courier');
         console.log(numberOfCouriers + " Couriers")
-        
         if(spawn.room.controller.level == 1){
             let minNumberHarvesters = 1 
             let minNumberCouriers = 2
